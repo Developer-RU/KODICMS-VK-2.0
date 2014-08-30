8c5cbd785d175cfc34ce404aff686623c898fec2
Павел <p.masyukov@yandex.ru>
2014-08-31 01:13:35 +0600

    Обновил шаблон

--- views/widgets/frontend/vkgroup.php
+++ views/widgets/frontend/vkgroup.php
@@ -1,4 +1,5 @@
 <script type="text/javascript" src="//vk.com/js/api/openapi.js?115"></script>
+<!-- VK Widget -->
 <div id="vk_groups"></div>
 <script type="text/javascript">
     VK.Widgets.Group("vk_groups", {
@@ -10,4 +11,6 @@
         color3: "<?php echo $plugin->get('btncolor'); ?>"
     }, <?php echo $plugin->get('group'); ?>);
 </script>
-<noscript>Please enable JavaScript to view the widget Vkgroup.</a></noscript>
+<noscript>Please enable JavaScript to view the widget Vkgroup.</a></noscript><br>
+<p><b>Написать разработчику:  </b>   <a href="mailto:p.masyukov@yandex.ru" class="dsq-brlink">p.masyukov@yandex.ru</a></p>
+<p><b>Последняя версия с GitHub: </b>   <code>https://github.com/Developer-RU/Kodicms-plugin-vkgroup.git</code></p><br>
